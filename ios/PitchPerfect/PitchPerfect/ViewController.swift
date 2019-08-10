0f266cfef45157fdc107929b85d8b070931e529b
Admin <admin@Mac-Admin.local>
2019-08-10 23:39:44 +0300

    Init hello world app

---
+++ ios/PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Admin on 10/08/2019.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
