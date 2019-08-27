633553e4ec3867466fdb1ba5357d8530306db22e
Aleksander Sadov <aleksandersadov95@gmail.com>
2019-08-27 14:57:38 +0700

    Android continue working on sunshine

--- android/ud851-Sunshine/S11.03-Exercise-DetailLayoutAndDataBinding/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ android/ud851-Sunshine/S11.03-Exercise-DetailLayoutAndDataBinding/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -246,6 +246,7 @@ public class DetailActivity extends AppCompatActivity implements
         }
 
 //      TODO (7) Display the weather icon using mDetailBinding
+        mDetailBinding.
 
         /****************
          * Weather Date *
